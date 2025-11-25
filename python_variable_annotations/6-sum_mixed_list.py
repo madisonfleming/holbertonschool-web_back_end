cd85c1a37d6b1bf5155ae624bd2de9b7a687fff1
madisonfleming <maddyyfleming@gmail.com>
2025-11-25 11:57:04 +1100

    7

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -7,7 +7,8 @@ import typing
 
 
 def sum_mixed_list(mxd_lst: typing.List[typing.Union[int, float]]) -> float:
-    """type annotated function to return sum of float and int arguments in list"""
+    """type annotated function to return sum of
+    float and int arguments in list"""
     sum: float = 0.0
     for i in mxd_lst:
         sum = sum + i
