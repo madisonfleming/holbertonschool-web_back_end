77c8340dbda9754a91891b1e2b9f7210d94d34c0
madisonfleming <maddyyfleming@gmail.com>
2025-12-11 15:49:50 +1100

    3

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -40,6 +40,7 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> dict:
+        """get hyper index"""
         dataset = self.indexed_dataset()
         assert 0 <= index < len(dataset)
         data = []
