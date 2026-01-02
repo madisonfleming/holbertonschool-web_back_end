8fb073709d43f87f1e71be27392339c6139de7e8
madisonfleming <maddyyfleming@gmail.com>
2026-01-02 19:34:46 +1100

    10

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,3 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  return array.map(value => appendString + value);
+const result = [];
+for (const value of array) {
+    result.push(appendString + value);
+}
+return result;
 }
