33a648dc3a74216cb8f319c0c8fb43a765038e1a
madisonfleming <maddyyfleming@gmail.com>
2026-01-02 19:44:20 +1100

    12

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,14 @@
+export default function createReportObject(employeesList) {
+    return {
+        allEmployees: employeesList,
+        getNumberOfDepartments: (args) => {
+            let i = 0;
+            for (const arg in args) {
+                if (arg) {
+                    i += 1;
+                }
+            }
+            return i;
+        }
+    }
+}
