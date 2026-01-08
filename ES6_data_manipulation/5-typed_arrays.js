8d879df1db1fb41daeb5d3031f11d8d1971845a0
madisonfleming <maddyyfleming@gmail.com>
2026-01-08 17:14:23 +1100

    5

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
     const buffer = new ArrayBuffer(length)
     const view = new DataView(buffer);
-    if (position < 0 || position >= view.length) {
+    if (position < 0 || position >= length) {
         throw new Error('Position outside range');
     }
     view.setInt8(position, value);
