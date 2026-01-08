19661b7f33b87309bbb0ae89ee2ef9125b5d8c0c
madisonfleming <maddyyfleming@gmail.com>
2026-01-08 18:37:44 +1100

    10

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,5 +1,5 @@
 export default function updateUniqueItems(map) {
-    const updateMap = map.set('Pasta', '100')
+    const updateMap = map.set('Pasta', 100)
     .set('Rice', 100);
     return updateMap;
 }
