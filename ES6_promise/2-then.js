c98c98542b1a93f092c3056584ded3faa6483acb
madisonfleming <maddyyfleming@gmail.com>
2026-01-11 19:52:39 +1100

    2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,10 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-    return new Promise((resolve, reject) => {
-        if (promise) {
-            resolve({ status: 200, body: 'success' });
-            console.log("Got a response from the API")
-        } else {
-            reject(Error());
-        }
-    });
+    return promise
+    .then(() => {
+        console.log('Got a response from the API');
+        return { status: 200, body: 'success'};
+    })
+    .catch(() => Error());
 }
