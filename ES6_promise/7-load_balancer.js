80059cb5edcc34dadab8516f56f649bc59b0a0e2
madisonfleming <maddyyfleming@gmail.com>
2026-01-13 17:16:26 +1100

    7

--- ES6_promise/7-load_balancer.js
+++ ES6_promise/7-load_balancer.js
@@ -1,9 +1,3 @@
 export default function loadBalancer(chinaDownload, USDownload) {
     return Promise.race([chinaDownload, USDownload])
-    .then((value) => {
-        console.log(value);
-    }),
-    (error) => {
-        console.error(error);
-    }
 }
