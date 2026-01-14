35660a7386e3255f899ff8646552d2fc1d212a43
madisonfleming <maddyyfleming@gmail.com>
2026-01-14 15:23:44 +1100

    1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+const readline = require('node:readline');
+
+const r1 = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+process.on('exit', () => {
+  console.log('This important software is now closing');
+  process.exit();
+});
+r1.question('Welcome to Holberton School, what is your name? ', (name) => {
+  console.log(`Your name is: ${name}`);
+  r1.close();
+});
