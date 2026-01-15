daadf83084e532aaaf642a1dcce67c9c104efd05
madisonfleming <maddyyfleming@gmail.com>
2026-01-15 15:19:58 +1100

    1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,17 +1,9 @@
-const readline = require('node:readline');
-
-const r1 = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-process.on('SIGINT', () => {
-  console.log('This important software is now closing');
-  process.exit();
-});
-
 process.stdout.write('Welcome to Holberton School, what is your name?');
 
-r1.question('', (name) => {
-  console.log(`Your name is: ${name}`);
-  r1.close();
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  process.stdout.write(`Your name is: ${name}`);
+});
+process.stdin.on('end', () => {
+    process.stdout.write('This important software is now closing');
 });
