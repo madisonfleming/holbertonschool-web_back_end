046de9787a720b5c53f5cacec336a23226ddeffa
madisonfleming <maddyyfleming@gmail.com>
2026-01-15 15:24:08 +1100

    1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,5 +5,5 @@ process.stdin.on('readable', () => {
   if (name !== null) process.stdout.write(`Your name is: ${name}`);
 });
 process.stdin.on('end', () => {
-    process.stdout.write('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
