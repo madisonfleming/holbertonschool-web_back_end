185284bef37e027028da49ae7334c1fb9041b14d
madisonfleming <maddyyfleming@gmail.com>
2026-01-16 12:18:59 +1100

    4

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,11 +1,16 @@
-const { createServer } = require('node:http');
+const http = require('http');
 
 const port = 1245;
-const app = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
-app.listen(port, () => {
-  console.log(`Server running at http://${port}/`);
-});
+app.listen(port);
+module.exports = app;
