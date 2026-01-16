b747a6684c9ed1102f54487cbe3e38bb1b063a46
madisonfleming <maddyyfleming@gmail.com>
2026-01-16 12:24:18 +1100

    4

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -3,7 +3,7 @@ const http = require('http');
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-  if (req.url === '/') {
+  if (req.url.includes('/')) {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello Holberton School!');
